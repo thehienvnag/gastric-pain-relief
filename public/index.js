ce902e0faf748aa35d00df21d3383bd0fb8c17f7
ivanhswe <ivanhswe@gmail.com>
2023-05-13 10:20:56 +0700

    Fix toggle bar

--- public/index.js
+++ public/index.js
@@ -32,4 +32,16 @@ createApp({
             ],
         }
     },
+    mounted() {
+        var navToggle = document.getElementById("nav-toggle");
+        var navContent = document.getElementById("nav-content");
+
+        navToggle.addEventListener("click", function () {
+            if (navContent.classList.contains("hidden")) {
+                navContent.classList.remove("hidden");
+            } else {
+                navContent.classList.add("hidden");
+            }
+        });
+    }
 }).mount('#app');
